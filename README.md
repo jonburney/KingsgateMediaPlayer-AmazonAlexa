02ff716d310a3ea563226e7fa6468841fbe9a1b7
Jon Burney <jon@version7.co.uk>
2016-10-18 22:31:50 +0100

    Added readme and set the correct index value for playback

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # KingsgateMediaPlayer-AmazonAlexa
+
+[![Build Status](https://travis-ci.org/jonburney/KingsgateMediaPlayer-AmazonAlexa.svg?branch=master)](https://travis-ci.org/jonburney/KingsgateMediaPlayer-AmazonAlexa)
+
+An Amazon Alexa skill that lets you listen to sermons from Kingsgate Community Church using your Amazon Echo.
--- index.js
+++ index.js
@@ -25,45 +25,48 @@ var controller = require("controller");
 
 function getLatestSermon(self, controller) {
 	let rssPath = 'http://www.kingsgateuk.com/Media/rss.xml';
-	
+
 	self.attributes['playOrder'] = 0
-	
+
 
     feed(rssPath, function (error, articles) {
-		var lastArticle = articles[articles.length-1];
-		var mediaUrl = lastArticle.enclosure.replace("http://", "https://");
 
-		console.log("author: " + lastArticle.author + "\nTitle: " + lastArticle.title + "\nURL: " + mediaUrl);
+        var lastArticleIndex = articles.length - 1;
+    	var lastArticle = articles[lastArticleIndex];
+    	var mediaUrl = lastArticle.enclosure.replace("http://", "https://");
 
-		self.attributes['playOrder'] = 0
-		self.attributes['index'] = 0;
-		self.attributes['offsetInMilliseconds'] = 0;
-		self.attributes['loop'] = true;
-		self.attributes['shuffle'] = false;
-		self.attributes['playbackIndexChanged'] = true;
-		self.attributes['audioStream'] = {
-			title: lastArticle.author + " " + lastArticle.title,
-			url: mediaUrl
-		};
-		self.handler.state = '';
-        
+    	console.log("author: " + lastArticle.author + "\nTitle: " + lastArticle.title + "\nURL: " + mediaUrl);
+
+        self.attributes["articleCount"] = articles.length;
+    	self.attributes['playOrder'] = 0
+    	self.attributes['index'] = lastArticleIndex;
+    	self.attributes['offsetInMilliseconds'] = 0;
+    	self.attributes['loop'] = true;
+    	self.attributes['shuffle'] = false;
+    	self.attributes['playbackIndexChanged'] = true;
+    	self.attributes['audioStream'] = {
+    		title: lastArticle.author + " " + lastArticle.title,
+    		url: mediaUrl
+    	};
+    	self.handler.state = '';
+
 		controller.play.call(self);
-    });   
+    });
 }
 
 
 var handlers = {
-	
+
 	startModeIntentHandlers : Alexa.CreateStateHandler('', {
-		
+
 		'Unhandled': function() {
 			console.log("Unable to map to intent via startModeIntentHandlers. Handler state = " + this.handler.state);
-			
+
 			var message = "Sorry, I didn't understand your request. Please say, play the latest sermon to listen to the latest sermon.";
 			this.response.speak(message).listen(message);
 			this.emit(":responseReady");
 		},
-		
+
 		'LaunchRequest' : function () {
             this.attributes['playOrder'] = Array.apply(null, {length: audioData.length}).map(Number.call, Number);
             this.attributes['index'] = 0;
@@ -79,17 +82,17 @@ var handlers = {
             this.response.speak(message).listen(reprompt);
             this.emit(':responseReady');
 		},
-		
+
 		'PlayLatestSermon' : function () {
 			getLatestSermon(this, controller);
 		}
-		
+
 	}),
-	
+
 	playModeIntentHandlers : Alexa.CreateStateHandler("_PLAY_MODE", {
 		'Unhandled': function() {
 			console.log("Unable to map to intent via playModeIntentHandlers. Handler state = " + this.handler.state);
-			
+
 			var message = "Sorry, I didn't understand your request. Please say, play the latest sermon to listen to the latest sermon.";
 			this.response.speak(message).listen(message);
 			this.emit(":responseReady");
@@ -112,43 +115,45 @@ var handlers = {
 			this.attributes['enqueuedToken'] = false;
 			this.emit(':saveState', true);
 		},
-		'PlaybackStopped' : function () {        
+		'PlaybackStopped' : function () {
 			this.attributes['index'] = 0;
 			this.attributes['offsetInMilliseconds'] = this.event.request.offsetInMilliseconds;
 			this.emit(':saveState', true);
 		},
-		'AMAZON.NextIntent' : function () { 
-			controller.playNext.call(this) 
+		'AMAZON.NextIntent' : function () {
+			controller.playNext.call(this)
 		},
-        'AMAZON.PreviousIntent' : function () { 
-			controller.playPrevious.call(this) 
+        'AMAZON.PreviousIntent' : function () {
+			controller.playPrevious.call(this)
 		},
-        'AMAZON.PauseIntent' : function () { 
-			controller.stop.call(this) 
+        'AMAZON.PauseIntent' : function () {
+			controller.stop.call(this)
 		},
-        'AMAZON.StopIntent' : function () { 
-			controller.stop.call(this) 
+        'AMAZON.StopIntent' : function () {
+			controller.stop.call(this)
 		},
-        'AMAZON.CancelIntent' : function () { 
-			controller.stop.call(this) 
+        'AMAZON.CancelIntent' : function () {
+			controller.stop.call(this)
 		},
-        'AMAZON.ResumeIntent' : function () { 
-			controller.play.call(this) 
+        'AMAZON.ResumeIntent' : function () {
+
+            // Intent should speak "Resuming..." instead of "Playing..."
+			controller.play.call(this)
 		},
-        'AMAZON.LoopOnIntent' : function () { 
-			controller.loopOn.call(this) 
+        'AMAZON.LoopOnIntent' : function () {
+			controller.loopOn.call(this)
 		},
-        'AMAZON.LoopOffIntent' : function () { 
-			controller.loopOff.call(this) 
+        'AMAZON.LoopOffIntent' : function () {
+			controller.loopOff.call(this)
 		},
-        'AMAZON.ShuffleOnIntent' : function () { 
-			controller.shuffleOn.call(this) 
+        'AMAZON.ShuffleOnIntent' : function () {
+			controller.shuffleOn.call(this)
 		},
-        'AMAZON.ShuffleOffIntent' : function () { 
-			controller.shuffleOff.call(this) 
+        'AMAZON.ShuffleOffIntent' : function () {
+			controller.shuffleOff.call(this)
 		},
-		'AMAZON.StartOverIntent' : function () { 
-			controller.restart.call(this) 
+		'AMAZON.StartOverIntent' : function () {
+			controller.restart.call(this)
 		}
 	})
 
@@ -156,7 +161,7 @@ var handlers = {
 
 
 exports.handler = (event, context, callback) => {
-	
+
 	var alexa = Alexa.handler(event, context);
 	alexa.appId = "amzn1.ask.skill.74c0e160-0e95-48f8-936b-f6134db15077";
 	alexa.dynamoDBTableName = "AlexaKingsgateSkillTable";
@@ -166,4 +171,3 @@ exports.handler = (event, context, callback) => {
 	);
 	alexa.execute();
 };
-
