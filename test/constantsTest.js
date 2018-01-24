536dfdeba511d414f65663a76233f3a21a1e4cd8
Jon Burney <jon@version7.co.uk>
2018-01-24 09:32:42 +0000

    Added strict mode to tests

--- test/constantsTest.js
+++ test/constantsTest.js
@@ -16,6 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
+'use strict';
+
 var assert = require('assert');
 var constants = require("../constants");
 
--- test/indexTest.js
+++ test/indexTest.js
@@ -16,5 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
+'use strict';
 
 var assert = require('assert');
