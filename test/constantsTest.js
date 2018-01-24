75757bf7a08042236f0ea0e676f3112625a1621f
Burney <burneyjb@amazon.com>
2018-01-24 09:27:40 +0000

    initial unit tests

---
+++ test/constantsTest.js
@@ -0,0 +1,49 @@
+/*
+ * Kingsgate Media Player
+ * Copyright (C) 2018 Jon Burney (jon@version7.co.uk)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+var assert = require('assert');
+var constants = require("../constants");
+
+// target validation data
+var targetData = {
+    appId: "amzn1.ask.skill.74c0e160-0e95-48f8-936b-f6134db15077",
+    dynamoDbTable: "AlexaKingsgateSkillTable",
+    rssFeedUrl: "http://www.kingsgateuk.com/Media/rss.xml"
+};
+
+// Tests
+describe("constants", function() {
+
+    describe("appId", function() {
+        it('appId should equal: ' + targetData.appId, function() {
+            assert.equal(constants.appId, targetData.appId);
+        });
+    });
+    
+    describe("dynamoDbTable", function() {
+        it('dynamoDbTable should equal: ' + targetData.dynamoDbTable, function() {
+            assert.equal(constants.dynamoDbTable, targetData.dynamoDbTable);
+        });
+    });
+
+    describe("rssFeedUrl", function() {
+        it('rssFeedUrl should equal: ' + targetData.rssFeedUrl, function() {
+            assert.equal(constants.rssFeedUrl, targetData.rssFeedUrl);
+        });
+    });
+});
--- test/controllerTest.js
+++ test/controllerTest.js
@@ -18,3 +18,31 @@
  */
 
 var assert = require('assert');
+var sinon = require('sinon');
+var controller = require("../controller");
+
+
+
+describe("controller", function() {
+    before(function(){
+        this.attributes = [];
+        
+    });
+
+    describe("pause", function() {
+        it('Pause calls audioPlayerStop', function() {
+            let response = {
+                audioPlayerStop: sinon.stub()
+            }
+
+            let attributes = {
+                offsetInMilliseconds: sinon.stub()
+            }
+            
+            
+            //sinon.stub(this.response, 'audioPlayerStop');
+            
+            controller.pause(this);
+        });
+    });
+});
